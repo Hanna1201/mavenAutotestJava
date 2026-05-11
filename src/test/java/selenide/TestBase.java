35f39da3c1451c0bc802eefdd203bdf6b1c804f4
anna <hannakavaliova1201@gmail.com>
2026-05-11 11:42:46 +0300

    Add ReportPortal integration

--- src/test/java/selenide/TestBase.java
+++ src/test/java/selenide/TestBase.java
@@ -1,13 +1,16 @@
 package selenide;
 
 import com.codeborne.selenide.Configuration;
+import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
+@Listeners(ReportPortalTestNGListener.class)
 public class TestBase {
 
     @BeforeMethod
